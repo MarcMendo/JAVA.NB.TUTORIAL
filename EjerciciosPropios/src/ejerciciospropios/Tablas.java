866bca48844d0e86e64562ddaa91c98fb804fda3
Carlos Eduardo <Carlos Eduardo@192.168.1.4>
2019-01-10 19:25:15 -0500

    Primer update: - Ejercicio Tablas, cambio autor

--- EjerciciosPropios/src/ejerciciospropios/Tablas.java
+++ EjerciciosPropios/src/ejerciciospropios/Tablas.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejerciciospropios;
 
 import javax.swing.table.DefaultTableModel;
 
 /**
- *
- * @author Carlos Eduardo
+ ** @author CABA Soft
  */
 public class Tablas extends javax.swing.JFrame {
     
